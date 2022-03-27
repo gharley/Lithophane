b5e8cd69cb857caf926780d2f1bf125125302868
Greg Harley <greg@harleyhood.com>
2022-03-27 14:45:31 -0600

    Learning numpy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import time
 
+import matplotlib.image
 import numpy as np
 
 from matplotlib.backends.qt_compat import QtWidgets
@@ -25,39 +26,48 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         # self.setCentralWidget(self._main)
         # layout = QtWidgets.QVBoxLayout(self._main)
         #
-        img = self._to_gray_scale(Image.open("C:/Cloud/Google/Fab/Artwork/nsfw.png"))
-        dpi = img.info['dpi'][0]
-        fig = Figure(figsize=(img.width / dpi, img.height / dpi))
-        fig.figimage(img, cmap='gray')
+        self.process_image("C:/Cloud/Google/Fab/Artwork/nsfw.png")
+        self.imgLayout = self._main.imgLayout
+        # scale = 330 / img.width
+        # img = ImageOps.scale(img, scale, True)
+        # dpi = img.info['dpi']
+        fig = Figure(figsize=(self._img.width, self._img.height))
+        fig.figimage(self._img, cmap='gray')
         static_canvas = FigureCanvas(fig)
         # # Ideally one would use self.addToolBar here, but it is slightly
         # # incompatible between PyQt6 and other bindings, so we just add the
         # # toolbar as a plain widget instead.
         # # layout.addWidget(NavigationToolbar(static_canvas, self))
         # layout.addWidget(static_canvas)
+        # self._main.imgLayout.addWidget(NavigationToolbar(static_canvas, self))
         self._main.imgLayout.addWidget(static_canvas)
         #
-        dynamic_canvas = FigureCanvas(Figure(figsize=(5, 3)))
-        self._main.imgLayout.addWidget(dynamic_canvas)
-        self._main.imgLayout.addWidget(NavigationToolbar(dynamic_canvas, self))
-
-        # self._static_ax = static_canvas.figure.subplots()
-        # t = np.linspace(0, 10, 501)
-        # self._static_ax.plot(t, np.tan(t), ".")
-
-        self._dynamic_ax = dynamic_canvas.figure.subplots()
-        t = np.linspace(0, 10, 101)
-        # Set up a Line2D.
-        self._line, = self._dynamic_ax.plot(t, np.sin(t + time.time()))
-        self._timer = dynamic_canvas.new_timer(50)
-        self._timer.add_callback(self._update_canvas)
-        self._timer.start()
+        # dynamic_canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        # self._main.imgLayout.addWidget(dynamic_canvas)
+        # self._main.imgLayout.addWidget(NavigationToolbar(dynamic_canvas, self))
+        #
+        # # self._static_ax = static_canvas.figure.subplots()
+        # # t = np.linspace(0, 10, 501)
+        # # self._static_ax.plot(t, np.tan(t), ".")
+        #
+        # self._dynamic_ax = dynamic_canvas.figure.subplots()
+        # t = np.linspace(0, 10, 101)
+        # # Set up a Line2D.
+        # self._line, = self._dynamic_ax.plot(t, np.sin(t + time.time()))
+        # self._timer = dynamic_canvas.new_timer(50)
+        # self._timer.add_callback(self._update_canvas)
+        # self._timer.start()
 
     def _update_canvas(self):
         t = np.linspace(0, 10, 101)
         # Shift the sinusoid as a function of time.
         self._line.set_data(t, np.sin(t + time.time()))
         self._line.figure.canvas.draw()
+
+    def process_image(self, filename):
+        self._img = Image.open(filename)
+        gray = ImageOps.grayscale(self._img)
+        self._heights = matplotlib.image.pil_to_array(gray) / np.max(gray)
 
     def _to_gray_scale(self, img: Image):
         # r, g, b = img[:, :, 0], img[:, :, 1], img[:, :, 2]
