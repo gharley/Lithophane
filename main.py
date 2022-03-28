63b1bd6f926ac555dbcb2ed1e4f4e4deb08ab29d
Greg Harley <greg@harleyhood.com>
2022-03-28 16:46:01 -0600

    working on mesh

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import time
 
 import matplotlib.image
 import numpy as np
+from stl import mesh
 
 from matplotlib.backends.qt_compat import QtWidgets
 from matplotlib.backends.backend_qt5agg import (
-    FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+    FigureCanvas)
 from matplotlib.figure import Figure
+from matplotlib import pyplot
+from mpl_toolkits import mplot3d
 from PIL import Image, ImageOps
 from PyQt5 import uic
 from PyQt5.QtCore import QFile
@@ -21,6 +24,8 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self._heights = None
         self._vectors = None
         self._faces = None
+        self._model = None
+
         self._max_height = 3
         self._max_size = 100
 
@@ -34,14 +39,29 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         # layout = QtWidgets.QVBoxLayout(self._main)
         #
         self.process_image("C:/Cloud/Google/Fab/Artwork/nsfw.png")
+
+        # Create a new plot
+        figure = pyplot.figure()
+        axes = mplot3d.Axes3D(figure)
+
+        # Load the STL files and add the vectors to the plot
+        axes.add_collection3d(mplot3d.art3d.Poly3DCollection(self._model.vectors))
+
+        # Auto scale to the mesh size
+        scale = self._model.points.flatten(-1)
+        axes.auto_scale_xyz(scale, scale, scale)
+
+        # Show the plot to the screen
+        pyplot.show()
+
         self.imgLayout = self._main.imgLayout
         fig = Figure(figsize=(self._img.width, self._img.height))
         fig.figimage(self._img, cmap='gray')
         static_canvas = FigureCanvas(fig)
         self._main.imgLayout.addWidget(static_canvas)
-        #
-        # dynamic_canvas = FigureCanvas(Figure(figsize=(5, 3)))
-        # self._main.imgLayout.addWidget(dynamic_canvas)
+
+        dynamic_canvas = FigureCanvas(Figure(figsize=(self._img.width, self._img.height)))
+        self.imgLayout.addWidget(dynamic_canvas)
         # self._main.imgLayout.addWidget(NavigationToolbar(dynamic_canvas, self))
         #
         # # self._static_ax = static_canvas.figure.subplots()
@@ -56,27 +76,34 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         # self._timer.add_callback(self._update_canvas)
         # self._timer.start()
 
-    def _update_canvas(self):
-        t = np.linspace(0, 10, 101)
-        # Shift the sinusoid as a function of time.
-        self._line.set_data(t, np.sin(t + time.time()))
-        self._line.figure.canvas.draw()
-
     def process_image(self, filename):
         self._img = Image.open(filename)
         gray = ImageOps.grayscale(self._img)
         scale = self._max_size / max(self._img.width, self._img.height)
         gray = ImageOps.scale(gray, scale, True)
-        self._heights = (matplotlib.image.pil_to_array(gray) / np.max(gray)) * self._max_height
-        self.get_vectors()
+        self._heights = np.zeros([gray.height + 2, gray.width + 2])
+        self._heights[1:-1, 1:-1] = (matplotlib.image.pil_to_array(gray) / np.max(gray.getdata())) * self._max_height
+        self._get_vectors()
+        self._get_faces()
 
-    def get_vectors(self):
-        height_index = 0
-        self._vectors = np.zeros((self._heights.size, 3), np.dtype(np.float64, (3,)))
+        # Create the mesh
+        self._model = mesh.Mesh(np.zeros(len(self._faces), dtype=mesh.Mesh.dtype))
+        for i, f in enumerate(self._vectors):
+            for j in range(3):
+                self._model.vectors[i][j] = f[j]
+
+    def _get_vectors(self):
+        self._vectors = np.zeros([self._heights.shape[0], self._heights.shape[1]], dtype=np.dtype((np.float64, 3)))
         for x in range(self._heights.shape[0]):
             for y in range(self._heights.shape[1]):
-                self._vectors[height_index] = (x, y, self._heights[x][y])
-                height_index += 1
+                self._vectors[x, y] = (x, y, self._heights[x][y])
+
+    def _get_faces(self):
+        self._faces = []
+        for x in range(self._vectors.shape[0] - 1):
+            for y in range(self._vectors.shape[1] - 1):
+                self._faces.append((x, y, y + 1))
+                self._faces.append((x, y + 1, y))
 
 
 if __name__ == "__main__":
