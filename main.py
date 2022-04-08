a8f29000d792d5f0ff29242d93b30ed8c37fdcab
Greg Harley <greg@harleyhood.com>
2022-04-08 17:17:14 -0600

    beating my head against a wall

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self._model = None
 
         self._base_height = 0
-        self._max_height = 5
+        self._max_height = 5.0
         self._max_size = 127
 
         ui_file = QFile('mainwindow.ui')
@@ -54,6 +54,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
         scale = self._max_size / max(self._img.width, self._img.height)
         self._img = ImageOps.scale(self._img, scale, True)
+
         gray = ImageOps.grayscale(self._img)
         img = o3d.geometry.Image(matplotlib.image.pil_to_array(gray))
         self._heights = np.zeros([gray.height + 2, gray.width + 2])
@@ -61,39 +62,42 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
         self._get_vertices()
 
-        pcl = o3d.geometry.PointCloud()
+        pcd = o3d.geometry.PointCloud()
         point_cloud = np.asarray(self._vertices)
-        pcl.points = o3d.utility.Vector3dVector(point_cloud)
-        pcl.paint_uniform_color([0.5, 0.5, 1.0])
-        pcl.estimate_normals()
-        pcl.orient_normals_to_align_with_direction()
-        pcl = pcl.normalize_normals()
-        aabb = pcl.get_axis_aligned_bounding_box()
+        pcd.points = o3d.utility.Vector3dVector(point_cloud)
+        pcd.paint_uniform_color([0.5, 0.5, 1.0])
+        pcd.estimate_normals()
+        pcd = pcd.normalize_normals()
+        # o3d.visualization.draw_geometries([pcd], point_show_normal=True)
+        pcd.orient_normals_to_align_with_direction([0.0, 0.0, 1.0])
+        aabb = pcd.get_axis_aligned_bounding_box()
         aabb.color = (1, 0, 0)
-        obb = pcl.get_oriented_bounding_box()
+        obb = pcd.get_oriented_bounding_box()
         obb.color = (0, 1, 0)
-        o3d.visualization.draw_geometries([pcl, aabb, obb], mesh_show_back_face=True)
-        voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcl, voxel_size=0.40)
-        o3d.visualization.draw_geometries([voxel_grid], mesh_show_back_face=True)
+        o3d.visualization.draw_geometries([pcd], point_show_normal=True)
+        # voxel_grid = o3d.geometry.VoxelGrid.create_dense([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], 0.4, float(self._img.width), float(self._img.height), self._max_height)
+        # voxel_grid = voxel_grid.carve_depth_map(img, o3d.camera.PinholeCameraParameters().extrinsic)
+        # o3d.visualization.draw_geometries([voxel_grid, pcd], mesh_show_back_face=True)
         poisson = True
 
         if poisson:  # Mesh from poisson
-            bpa_mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcl, depth=10, linear_fit=False, n_threads=-1)
+            bpa_mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=10, scale=1.0, linear_fit=True, n_threads=-1)
 
         else:  # Mesh from ball pivot
-            distances = pcl.compute_nearest_neighbor_distance()
+            distances = pcd.compute_nearest_neighbor_distance()
             avg_dist = np.mean(distances)
             radius = avg_dist * 3
-            bpa_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcl, o3d.utility.DoubleVector([radius, radius * 2]))
+            bpa_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd, o3d.utility.DoubleVector([radius, radius * 2]))
 
         bpa_mesh = bpa_mesh.compute_vertex_normals()
         bpa_mesh = bpa_mesh.compute_triangle_normals()
-        # bpa_mesh.remove_degenerate_triangles()
-        # bpa_mesh = bpa_mesh.compute_convex_hull()[0]
-        o3d.visualization.draw_geometries([bpa_mesh], mesh_show_back_face=True)
+        bpa_mesh.remove_degenerate_triangles()
+        # bpa_mesh = bpa_mesh.remove_non_manifold_edges()
+        # bpa_mesh.filter_smooth_taubin()
+        o3d.visualization.draw_geometries([bpa_mesh], mesh_show_back_face=False)
         print(f'mesh.is_edge_manifold = {bpa_mesh.is_edge_manifold()}')
         print(f'mesh.is_vertex_manifold = {bpa_mesh.is_vertex_manifold()}')
-        print(f'mesh.is_watertight = {bpa_mesh.is_watertight()}')
+        # print(f'mesh.is_watertight = {bpa_mesh.is_watertight()}')
         # bpa_mesh = self._simplify_mesh(bpa_mesh)
         o3d.io.write_triangle_mesh("C:/Cloud/Google/Fab/Artwork/nsfw.stl", bpa_mesh)
 
@@ -103,18 +107,14 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         base = []
         for x in range(self._heights.shape[0]):
             for y in range(self._heights.shape[1]):
-                if 0 < x < self._heights.shape[0] - 1 and 0 < y < self._heights.shape[1] - 1:
-                    height = self._heights[x][y] + self._base_height
-                    while height >= self._base_height:
-                        vertices.append((float(x), float(y), height))
-                        height -= 1.0
+                base_height = self._base_height
+                height = self._heights[x][y] + base_height
+                # vertices.append((float(x), float(y), 0.0))
+                vertices.append((float(x), float(y), height))
+                # base.append((float(x), float(y), 0.0))
 
-                    base.append((float(x), float(y), self._base_height))
-                else:
-                    vertices.append((float(x), float(y), self._max_height))
-                    base.append((float(x), float(y), self._base_height))
-
-        self._vertices = np.append(np.array(vertices), np.array(base), axis=0)
+        self._vertices = np.array(vertices)
+        # self._vertices = np.append(np.array(vertices), np.array(base), axis=0)
 
     @staticmethod
     def _simplify_mesh(mesh_to_simplify):
